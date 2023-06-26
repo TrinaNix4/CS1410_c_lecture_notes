1dc2481d60da74c86204a7a96751c52733e9f47d
Trina Nixon <TrinaNixon7@gmail.com>
2023-06-26 11:33:42 -0600

    practice program

---
+++ PieCalc.py
@@ -0,0 +1,12 @@
+'''
+Author: Trina Nixon
+Brief: Pie Calculator to calculate quarts and pints for a pie
+
+'''
+
+PI = 3.14
+QUART_INCHES = 69.3549
+PINT_INCHES = 34.7664
+
+diameter = float(input("Enter the pie pan diameter: "))
+height = float(input("Enter the height of pie pan: "))
